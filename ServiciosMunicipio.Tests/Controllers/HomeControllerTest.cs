2b8e83b4e918a4bd3ea9d4699d01e84316c00664
Test-Ibanez-350 <81836524+Test-Ibanez-350@users.noreply.github.com>
2025-05-27 09:41:37 -0600

    Tests

---
+++ ServiciosMunicipio.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiciosMunicipio;
+using ServiciosMunicipio.Controllers;
+using System.Web.Mvc;
+
+namespace ServiciosMunicipio.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+
+            // Actuar
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
