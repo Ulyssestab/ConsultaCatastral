fc5c5f70d928fd4ad97660ede96e94d42a0eaeae
Test-Ibanez-350 <ibanez_rg_350@hotmail.com>
2025-06-09 13:57:45 -0600

    MG-8
    Servicios

--- ServiciosMunicipio.Tests/Controllers/HomeControllerTest.cs
+++ ServiciosMunicipio.Tests/Controllers/HomeControllerTest.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ServiciosMunicipio;
 using ServiciosMunicipio.Controllers;
+using ServiciosMunicipio.Models;
+using ServiciosMunicipio.Utilerias;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System;
 
 namespace ServiciosMunicipio.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+        private string nombre = "Administrador";
+        private string pass = "66FAEE583898CC373ED7EC34FC887DFF34809195";
+        private JSONparser util = new JSONparser();
+
         [TestMethod]
-        public void Index()
+        public void IndexTest()
         {
             // Disponer
             HomeController controller = new HomeController();
@@ -20,6 +28,62 @@ namespace ServiciosMunicipio.Tests.Controllers
             // Declarar
             Assert.IsNotNull(result);
             Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+
+        [TestMethod]
+        public void AccesoTest()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+            
+
+            // Actuar
+            System.Web.Mvc.JsonResult result = (System.Web.Mvc.JsonResult)controller.Acceso(nombre);
+            var data = result.Data;
+            var serializer = new JavaScriptSerializer();
+            var jsonString = serializer.Serialize(data);
+            
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("true", jsonString);
+        }
+
+
+        [TestMethod]
+        public void AccesoUsuarioPerfilTest()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+            Usuario variable = new Usuario();
+
+            // Actuar
+            System.Web.Mvc.JsonResult result = (System.Web.Mvc.JsonResult)controller.AccesoUsuarioPerfil(nombre);
+            var data = result.Data;
+            var serializer = new JavaScriptSerializer();
+            var jsonString = serializer.Serialize(data);
+            
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", jsonString.Replace("\"", ""));
+        }
+
+        [TestMethod]
+        public void ObtenerAccesoTest()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+            Usuario variable = new Usuario();
+           
+            // Actuar
+            System.Web.Mvc.JsonResult result = (System.Web.Mvc.JsonResult)controller.ObtenerAcceso(nombre, pass);
+            var data = result.Data;
+            var serializer = new JavaScriptSerializer();
+            var jsonString = serializer.Serialize(data);
+
+            variable = jsonString != "[]" ? util.crearObjetoUsuario(jsonString.Replace("[", "").Replace("]", "")) : variable;
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("EL_Usuario", variable.NombreUsuario);
         }
     }
 }
